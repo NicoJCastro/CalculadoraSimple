1d1512a72763e9b33d609d9791eda267e20d7d4a
ncastro@vozy.co <ncastro@vozy.co>
2024-06-01 23:28:08 -0300

    correccion

--- dictionaries.py
+++
@@ -1,9 +0,0 @@
-houses = {
-    "Harry": "Gryffindor",
-    "Draco": "Slytherin"
-    }
-
-houses ["Hermione"] = "Gryffindor"
-
-if houses.get("Hermione") == houses.get("Harry") == "Gryffindor":
-    print("True")
--- square.py
+++
@@ -1,5 +0,0 @@
-# from functions import square
-import functions
-
-for i in range (10):
-    print(f"The square of {i} is {functions.square(i)}")
